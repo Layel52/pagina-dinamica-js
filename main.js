70b67370f7d395346790793c61e8049dbb4c84ae
Layel <layelcode@gmail.com>
2022-08-01 14:37:53 -0300

    Otimizando o main.js e ajustando o som de cada botão

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 
@@ -9,7 +9,18 @@ let contador = 0;
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas [contador].onclick = tocaSomPom;
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    //---------------------
+
+    console.log(idAudio);
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 
     contador = contador + 1;
 
